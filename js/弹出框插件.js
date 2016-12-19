1cfa9003d278191d127cb0574bbd12d316fbeb7f
KingJA <kingjavip@gmail.com>
2016-12-19 13:47:10 +0800

    add jQuery plups

--- js/弹出框插件.js
+++ js/弹出框插件.js
@@ -2,35 +2,25 @@
  * Created by Administrator on 2016/12/15.
  */
 
+// 自执行匿名闭包函数
 ;(function ($) {
-
+    //集成元素
     $.fn.extend({
-        confirm:function (values) {
-
-            // var confirmTitle = $("#confirm_title");
-            // var confirmContent = $("#confirm_content");
-            // var confirmButton = $("#confirm_button");
-
-            
-            values=$.extend({
-                title:"提示",
-                content:"这是一个确认对话框",
-                confirmText:"确认"
-            },values);
-
-            console.info("title:"+values.title);
-            console.info("content:"+values.content);
-            console.info("confirmText:"+values.confirmText);
-            var confirm_dialog = $("<div id='confirm_dialog'><h3 id='confirm_title'>"+values.title+"</h3><p id='confirm_content'>"+values.content+"</p><button id='confirm_button'>"+values.confirmText+"</button></div>");
-            // confirmTitle.text(values.title);
-            // confirmContent.text(values.content);
-            // confirmButton.text(values.confirmText);
+        confirm: function (values) {
+            //设置默认值
+            values = $.extend(defaults, values);
+            var confirm_dialog = $("<div id='confirm_dialog'><h3 id='confirm_title'>" + values.title + "</h3><p id='confirm_content'>" + values.content + "</p><button id='confirm_button'>" + values.confirmText + "</button></div>");
+            //进行赋值
             $("body").append(confirm_dialog);
-            $(this).on("click",function () {
+            //按钮点击
+            $(this).on("click", function () {
                 confirm_dialog.show();
             });
-            
         }
     });
-
+    var defaults = {
+        title: "提示",
+        content: "这是一个确认对话框",
+        confirmText: "确认"
+    }
 })(jQuery);
